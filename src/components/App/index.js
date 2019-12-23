1735fcc304bb7159d395d31f79c88d1130b21bb2
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2019-12-23 17:46:36 +0300

    upload starter project

---
+++ src/components/App/index.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// ray test touch <
+import React, { lazy, Suspense, useEffect, useRef } from 'react';
+// ray test touch >
+// ray test touch <
+import { Route } from 'react-router-dom';
+// import { listen } from 'quicklink';
+
+import { listenWithRmanifest } from '../../utils';
+// import { Route } from '@components/Router';
+// ray test touch >
+import Footer from '@components/Footer';
+import Hero from '@components/Hero';
+import style from './index.module.css';
+const Home = lazy(() => import(/* webpackChunkName: "home" */ '@pages/Home'));
+const About = lazy(() => import(/* webpackChunkName: "about" */ '@pages/About'));
+const Article = lazy(() => import(/* webpackChunkName: "article" */ '@pages/Article'));
+const Blog = lazy(() => import(/* webpackChunkName: "blog" */ '@pages/Blog'));
+
+// ray test touch <
+const QuicklinkWrapper = ({ children }) => {
+	const ref = useRef(null);
+	useEffect(() => {
+		console.log('ray : ***** [App QuicklinkWrapper] rendering done ref.current => ', ref.current);
+		listenWithRmanifest();
+	}, [ref]);
+
+	return (
+		<div ref={ref}>
+			{children}
+		</div>
+	);
+};
+// ray test touch >
+
+const App = () => {
+	useEffect(() => {
+		// console.log('ray : ***** [App] rendering done');
+		// window.addEventListener('load', () => {
+		// 	console.log('ray : ***** [App] load event done');
+		// });
+	}, []);
+	return (
+		<div className={style.app}>
+			<Hero />
+			<main className={style.wrapper}>
+				<Suspense fallback={<div>Loading...</div>}>
+					{/* ray test touch < */}
+					{/* <Route path="/" exact component={Home} />
+					<Route path="/blog" exact component={Blog} />
+					<Route path="/blog/:title" component={Article} />
+					<Route path="/about" exact component={About} /> */}
+					<Route path="/" exact render={() => (
+						<QuicklinkWrapper>
+							<Home />
+						</QuicklinkWrapper>
+					)} />
+					<Route path="/blog" exact render={() => (
+						<QuicklinkWrapper>
+							<Blog />
+						</QuicklinkWrapper>
+					)} />
+					<Route path="/blog/:title" exact render={() => (
+						<QuicklinkWrapper>
+							<Article />
+						</QuicklinkWrapper>
+					)} />
+					<Route path="/about" exact render={() => (
+						<QuicklinkWrapper>
+							<About />
+						</QuicklinkWrapper>
+					)} />
+					{/* ray test touch > */}
+				</Suspense>
+			</main>
+			<Footer />
+		</div>
+	);
+};
+// ray test touch >
+
+export default App;
